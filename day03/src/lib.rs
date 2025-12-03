2eb47e52dff68386f9b8bada53e350216e5133d1
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2025-12-03 23:26:06 +0100

    day03: really we only need one implementation after all

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -43,37 +43,8 @@ impl Bank {
             .sum()
     }
 
-    fn select_indices_pt1(&self) -> Result<(usize, usize)> {
-        if self.0.len() < 2 {
-            return Err(eyre!("bank has too few batteries"));
-        }
-
-        let (left, _) = self
-            .0
-            .iter()
-            .enumerate()
-            // we rev here because max_by_key takes the last of any equally maximum elements,
-            // and for our algorithm's correctness we have to have the leftmost maximum value
-            //
-            // also note that we rev _after_ we enumerate, so that the indices correctly count down as we go
-            .rev()
-            .skip(1)
-            .max_by_key(|(_idx, value)| **value)
-            .expect("maxing a non-empty list always produces something");
-
-        let (right, _) = self
-            .0
-            .iter()
-            .enumerate()
-            .skip(left + 1)
-            .max_by_key(|(_idx, value)| **value)
-            .expect("maxing a non-empty list always produces something");
-
-        Ok((left, right))
-    }
-
-    fn select_indices_pt2(&self) -> Result<[usize; 12]> {
-        if self.0.len() < 12 {
+    fn select_indices<const N: usize>(&self) -> Result<[usize; N]> {
+        if self.0.len() < N {
             return Err(eyre!("bank has too few batteries"));
         }
 
@@ -83,14 +54,10 @@ impl Bank {
         // this kind of allocation-elimination microoptimization is honestly kind of pointless; this runs fast enough.
         // but on the other hand, it's a chance to learn something new, so...
 
-        // here are two types that I promise have the same memory layout
-        type Uninit = [MaybeUninit<usize>; 12];
-        type Init = [usize; 12];
-
         // initialization loop
-        let mut indices: Uninit = [MaybeUninit::uninit(); 12];
-        for index in 0..12 {
-            let mut initial_iter = self.0.iter().enumerate().rev().skip(12 - 1 - index);
+        let mut indices: [MaybeUninit<usize>; N] = [MaybeUninit::uninit(); N];
+        for index in 0..N {
+            let mut initial_iter = self.0.iter().enumerate().rev().skip(N - 1 - index);
             let mut adapted_iter;
             let iter = if index > 0 {
                 // SAFETY: we choose the indices in order and only refer to a previous one
@@ -110,15 +77,17 @@ impl Bank {
 
         // safety: initializing an array by element is literally in the MaybeUninit docs:
         // https://doc.rust-lang.org/stable/std/mem/union.MaybeUninit.html
-        Ok(unsafe { std::mem::transmute::<Uninit, Init>(indices) })
+        // and I'm basically stealing the implementation from the (unstable) `MaybeUninit::array_assume_init`:
+        // https://github.com/joboet/rust/blob/985071b08f5c03e4f18d43c15f3ea82395588a5e/library/core/src/mem/maybe_uninit.rs#L843
+        Ok(unsafe { (&indices as *const _ as *const [usize; N]).read() })
     }
 }
 
 pub fn part1(input: &Path) -> Result<()> {
     let total_output_joltage = parse::<Bank>(input)?
         .map(|bank| -> Result<_> {
-            let (left, right) = bank.select_indices_pt1()?;
-            let joltage = bank.joltage_from_indices([left, right]);
+            let indices = bank.select_indices::<2>()?;
+            let joltage = bank.joltage_from_indices(indices);
             Ok(joltage)
         })
         .try_fold(0, |acc, elem| -> Result<_> { Ok(elem? + acc) })?;
@@ -129,7 +98,7 @@ pub fn part1(input: &Path) -> Result<()> {
 pub fn part2(input: &Path) -> Result<()> {
     let total_output_joltage = parse::<Bank>(input)?
         .map(|bank| -> Result<_> {
-            let indices = bank.select_indices_pt2()?;
+            let indices = bank.select_indices::<12>()?;
             let joltage = bank.joltage_from_indices(indices);
             Ok(joltage)
         })
