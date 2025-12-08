6ba0840d2ea2a6ed25201c0a1dd5eb26b8a4f086
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2025-12-08 23:15:53 +0100

    day08: refactor to a more useful helper abstraction

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -1,6 +1,9 @@
 use aoclib::parse;
 use color_eyre::Result;
-use std::{collections::BTreeMap, path::Path};
+use std::{
+    collections::{BTreeMap, HashMap},
+    path::Path,
+};
 
 #[derive(
     Debug,
@@ -31,11 +34,24 @@ impl Point {
     }
 }
 
-/// Solve part 1
+/// Compute the circuit assignments for a given point set
 ///
-/// - compute distances between each point pair
-/// - considering point pairs in order by distance, connect them
-fn solve_part1(points: &[Point], connection_limit: usize) -> u64 {
+/// The assignments are a map whose key is the circuit ID and whose value is the set of indices in the `points`
+/// list which are assigned to this circuit.
+fn compute_circuit_assignments(
+    points: &[Point],
+    connection_limit: Option<usize>,
+) -> HashMap<usize, Vec<usize>> {
+    // each point starts assigned to its own circuit, where the circuit id is the index of that point in points
+    let mut circuit_assignments = (0..points.len()).collect::<Vec<_>>();
+    // but we also keep track of the indices assigned to each circuit
+    let mut indices_by_circuit = circuit_assignments
+        .iter()
+        .copied()
+        .enumerate()
+        .map(|(idx, circuit_id)| (circuit_id, vec![idx]))
+        .collect::<HashMap<_, _>>();
+
     // keep track of the pairs, by distance
     let mut pairs_by_distance = BTreeMap::<_, Vec<_>>::new();
     for (i, a) in points.iter().enumerate() {
@@ -44,55 +60,64 @@ fn solve_part1(points: &[Point], connection_limit: usize) -> u64 {
             pairs_by_distance.entry(distance).or_default().push((i, j));
         }
     }
-
-    // each point starts assigned to its own circuit, where the circuit id is the index of that point in points
-    let mut circuit_assignments = (0..points.len()).collect::<Vec<_>>();
-
-    for (i, j) in pairs_by_distance
+    // we might limit this iterator, or not, depending on the arguments
+    let mut pairs_iter = pairs_by_distance
         .values()
         .flat_map(|list| list.iter())
-        .take(connection_limit)
-        .copied()
-    {
-        // eprintln!("considering ({}, {})", points[j], points[i]);
+        .copied();
+    let mut limited_iter;
+    let iter = match connection_limit {
+        None => &mut pairs_iter as &mut dyn Iterator<Item = _>,
+        Some(connection_limit) => {
+            limited_iter = pairs_iter.take(connection_limit);
+            &mut limited_iter
+        }
+    };
+    for (i, j) in iter {
         if circuit_assignments[i] == circuit_assignments[j] {
             // not actually a new connection
             // skip further work
-            // eprintln!(
-            //     " already part of the same circuit: {}",
-            //     circuit_assignments[i]
-            // );
             continue;
         }
         // this _is_ a new connection, which we implement by reassigning the circuit
         // assignment for everything on the bigger circuit to the smaller circuit
-        // this is a linear time operation on the number of points
-        // we could improve on that by keeping a map of indices by circuit id,
-        // but for now I'd save the hassle
-        debug_assert_ne!(
-            i, j,
-            "we should never hit the same circuit twice in this loop"
-        );
         let bigger = circuit_assignments[i].max(circuit_assignments[j]);
         let smaller = circuit_assignments[i].min(circuit_assignments[j]);
-        // eprintln!(
-        //     " reassigning all `{bigger}` ({}) circuits to `{smaller}` ({})",
-        //     points[bigger], points[smaller]
-        // );
-        for circuit_assignment in circuit_assignments.iter_mut() {
-            if *circuit_assignment == bigger {
-                *circuit_assignment = smaller;
-            }
+
+        // we have to update both circuit_assignments and indices_by_circuit
+        // this is a 3-step process:
+        // 1. remove the indices for the bigger circuit
+        // 2. update all those indices in the assignments to the smaller
+        // 3. append the big indices to those for the smaller circuit
+        let mut big_indices = indices_by_circuit.remove(&bigger).expect(
+            "removing the bigger always works because circuit assignments are kept current",
+        );
+        for idx in big_indices.iter().copied() {
+            circuit_assignments[idx] = smaller;
         }
-        // eprintln!(" circuit assignments: {circuit_assignments:?}");
+        let small_indices = indices_by_circuit.get_mut(&smaller).expect(
+            "accessing the smaller always works because circuit assignments are kept current",
+        );
+        small_indices.append(&mut big_indices);
+
+        if indices_by_circuit.len() <= 1 {
+            break;
+        }
     }
 
-    // we need the product of the sizes of the greatest three circuits
-    let mut circuit_sizes = vec![0_u64; points.len()];
-    for circuit in circuit_assignments.iter().copied() {
-        circuit_sizes[circuit] += 1;
-    }
-    // eprintln!("circuit sizes: {circuit_sizes:?}");
+    indices_by_circuit
+}
+
+/// Solve part 1
+///
+/// - compute distances between each point pair
+/// - considering point pairs in order by distance, connect them
+fn solve_part1(points: &[Point], connection_limit: usize) -> u64 {
+    let indices_by_circuit = compute_circuit_assignments(points, Some(connection_limit));
+    let mut circuit_sizes = indices_by_circuit
+        .values()
+        .map(|indices| indices.len() as u64)
+        .collect::<Vec<_>>();
     circuit_sizes.sort();
     circuit_sizes.into_iter().rev().take(3).product()
 }
